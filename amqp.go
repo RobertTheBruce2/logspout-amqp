5a735368649fb5205cf0090c1f5069520bd51049
RobertTheBruce2 <42508183+RobertTheBruce2@users.noreply.github.com>
2018-08-19 10:04:52 +0300

    Update amqp.go

--- amqp.go
+++ amqp.go
@@ -86,3 +86,37 @@ func failOnError(err error, msg string) {
 		log.Fatalf("%s: %s", msg, err)
 	}
 }
+
+func processMessage(message *router.Message) *map[string]interface{} {
+	dockerInfo := DockerInfo{
+		Name:     message.Container.Name,
+		ID:       message.Container.ID,
+		Image:    message.Container.Config.Image,
+		Hostname: message.Container.Config.Hostname,
+	}
+
+	data := make(map[string]interface{})
+
+	// Try to parse JSON-encoded m.Data. If it wasn't JSON, create an empty object
+	// and use the original data as the message.
+	if err := json.Unmarshal([]byte(message.Data), &data); err != nil {
+		data["message"] = message.Data
+	}
+
+	for k, v := range fields {
+		data[k] = v
+	}
+
+	data["docker"] = dockerInfo
+
+	return &data
+}
+
+// Container Docker info for event data
+type DockerInfo struct {
+	Name     string `json:"name"`
+	ID       string `json:"id"`
+	Image    string `json:"image"`
+	Hostname string `json:"hostname"`
+}
+
